c004d7a2b2b5b0365e54f2e2ad8dd6dcd7de7439
Manuel Sanchez <manuelsfdelavega@gmail.com>
2025-04-02 22:46:19 -0300

    feat: change hello command to ping

--- src/algo1_bot/commands/general.py
+++ src/algo1_bot/commands/general.py
@@ -1,11 +1,10 @@
-import discord
-from discord import app_commands
+from discord import app_commands, Interaction
 
 from algo1_bot.commands.base_cog import BaseCog
 
 
 class General(BaseCog):
-    @app_commands.command(name="hello", description="Say hello!")
-    async def hello(self, interaction: discord.Interaction):
-        """A simple slash command"""
-        await interaction.response.send_message(f"Hi, {interaction.user.mention}!")
+    @app_commands.command(name="ping", description="Ping the bot")
+    async def ping(self, interaction: Interaction):
+        """A simple slash command that responds with 'Pong!'"""
+        await interaction.response.send_message("Pong!", ephemeral=True)
